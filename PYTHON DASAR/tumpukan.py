93306046f77722b52febeb4e4e3ae50befad288f
fadhluibnu <f.ibnu.a1@gmail.com>
2021-04-30 09:41:33 +0700

    'python'

---
+++ PYTHON DASAR/tumpukan.py
@@ -0,0 +1,18 @@
+#stack keluar di akhir
+#queues keluar di awal
+
+tumpukan = [1,2,3,4,5,6]
+
+#memasukkan data baru
+tumpukan.append(7)
+print('data masuk : ', 7)
+print('data sekarang : ', tumpukan)
+
+tumpukan.append(8)
+print('data masuk : ', 8)
+print('data sekarang : ', tumpukan)
+
+
+tumpukan.pop()#mengulangkan data paling belakang
+print('data sekarang : ', tumpukan)
+
