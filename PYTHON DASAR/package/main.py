dcdf4954ba337dabb863ff9e22ca81785280533b
fadhluibnu <f.ibnu.a1@gmail.com>
2021-05-01 09:13:29 +0700

    python

---
+++ PYTHON DASAR/package/main.py
@@ -0,0 +1,18 @@
+# from sains import fisika,matematika
+#
+# fisika.kecepatan(3,4)
+# matematika.tambah(4,5)
+
+# import sains
+# from sains import tambah
+# sains.tambah(3,4)
+
+# import sains
+# from sains import kecepatan
+# sains.kecepatan(50, 10)
+# sains.tambah(2,3)
+# sains.kurang(3,2)
+
+#contoh package
+import math
+print(math.cosh(20))
---
+++ PYTHON DASAR/package/sains/fisika.py
@@ -0,0 +1,7 @@
+def kecepatan(waktu, jarak):
+     print('menghitung kecepatan')
+     return  jarak/waktu
+
+def waktu_tempuh(kecepatan, jarak):
+     print('menghitung waktu tempuh')
+     return jarak/kecepatan
